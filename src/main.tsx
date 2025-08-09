dfedbabc6914d570824c9bcc5b4f07ede2c15dcd
Mahlon <whyesem11@gmail.com>
2025-08-09 16:43:03 -0400

    ai wroko

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { ConvexReactClient } from "convex/react";
 import "./index.css";
 import App from "./App";
 
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+const convex = new ConvexReactClient(process.env.PUBLIC_CONVEX_URL as string);
 
 createRoot(document.getElementById("root")!).render(
   <ConvexAuthProvider client={convex}>
