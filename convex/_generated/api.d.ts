66ae7771535b5f4ada4c395c3633a3247989cd78
MahlonVhiem <whyesem11@gmail.com>
2025-08-14 14:58:41 -0400

    Updated dataModel.d.ts

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,13 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as agora from "../agora.js";
+import type * as auth from "../auth.js";
+import type * as http from "../http.js";
+import type * as posts from "../posts.js";
+import type * as profiles from "../profiles.js";
+import type * as router from "../router.js";
+import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -22,7 +29,15 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  agora: typeof agora;
+  auth: typeof auth;
+  http: typeof http;
+  posts: typeof posts;
+  profiles: typeof profiles;
+  router: typeof router;
+  users: typeof users;
+}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
